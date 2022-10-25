ee317ef0be388edd8969d0934bc14c5f4427d7e4
Vinicius Victor <vine.victordev@gmail.com>
2022-10-25 01:37:29 -0300

    Função Trilhão,Bilhão e Milhão

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 var numero = prompt();
 
+
 var digitos = numero.split("");
+var contadorDigitosFaltando = digitos.length
 
 function DefinirTamanho() {
     var tamanho;
-    if (digitos.length >= 13) {
+    if (digitos.length >= 13 && digitos.length <= 15) {
         tamanho = "T";
     } else if (digitos.length >= 10 && digitos.length <= 12) {
         tamanho = "B"
@@ -12,7 +14,40 @@ function DefinirTamanho() {
         tamanho = "M"
     } else if (digitos.length <= 6) {
         tamanho = "Mil"
+    } else {
+        tamanho = "N"
     }
+    return tamanho;
 };
+
+function Trilhao() {
+    var trilhao = ""
+    if (digitos.length == 13 && digitos[0] == 1) {
+        var trilhao = "Trilhão"
+    } else {
+        var trilhao = "Trilhões"
+    }
+    return trilhao;
+}
+
+function Bilhao() {
+    var bilhao = ""
+    if (digitos.length == 10 && digitos[0] == 1) {
+        var bilhao = "Bilhão"
+    } else {
+        var bilhao = "Bilhões"
+    }
+    return bilhao;
+}
+
+function Milhao() {
+    var milhao = ""
+    if (digitos.length == 7 && digitos[0] == 1) {
+        var milhao = "Milhão"
+    } else {
+        var milhao = "Milhões"
+    }
+    return milhao;
+}
 
 
